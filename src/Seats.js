19e8956d641ecfdc1ce78813a59eed2c0be7a587
Caio-Coldebella <c232621@dac.unicamp.br>
2022-07-31 18:56:45 -0300

    ajuste de layout da pagina de selecao de assentos

--- src/Seats.js
+++ src/Seats.js
@@ -24,12 +24,28 @@ export default function Seats(){
     return(
         <>
         <PageTop><p>Selecione o(s) assento(s)</p></PageTop>
-        <SEATSBOARD>{seats.map( seat => {return <SeatCircle set={setSelectedseats} seats={selectedseats} name={seat.name} isAvailable={seat.isAvailable}></SeatCircle>})}</SEATSBOARD>
-        <form onSubmit={sendData}>
-            <input type="text" value={personName} required onChange={e => setPersonName(e.target.value)} placeholder="Digite seu nome..."/>
-            <input type="text" value={cpf} required onChange={e => setCpf(e.target.value)} placeholder="Digite seu CPF..."/>
-            <button type="submit">Reservar assento(s)</button>
-        </form>
+        <SEATSBOARD>{seats.map( seat => {return <SeatCircle set={setSelectedseats} seats={selectedseats} name={seat.name} isAvailable={seat.isAvailable}/>})}</SEATSBOARD>
+        <SEATSEXAMPLE>
+            <EXAMPLE>
+                <CIRCLE color={"#8DD7CF"}></CIRCLE>
+                Selecionado
+            </EXAMPLE>
+            <EXAMPLE>
+                <CIRCLE color={"#C3CFD9"}></CIRCLE>
+                Disponível
+            </EXAMPLE>
+            <EXAMPLE>
+                <CIRCLE color={"#FBE192"}></CIRCLE>
+                Indisponível
+            </EXAMPLE>
+        </SEATSEXAMPLE>
+        <FORM onSubmit={sendData}>
+            Nome do comprador:
+            <INPUTFORM type="text" value={personName} required onChange={e => setPersonName(e.target.value)} placeholder="Digite seu nome..."/>
+            CPF do comprador:
+            <INPUTFORM type="text" value={cpf} required onChange={e => setCpf(e.target.value)} placeholder="Digite seu CPF..."/>
+            <BUTTONFORM type="submit">Reservar assento(s)</BUTTONFORM>
+        </FORM>
         </>
     );
 }
@@ -40,6 +56,63 @@ const SEATSBOARD = styled.div`
     width: 100%;
     height: auto;
     row-gap: 18px;
-    column-gap: 9px;
+    column-gap: 10px;
     margin: 0 24px 0 24px;
+`;
+const SEATSEXAMPLE = styled.div`
+    display: flex;
+    width: auto;
+    height: auto;
+    justify-content: center;
+    margin: 15px 0 50px 0;
+    column-gap: 30px;
+`;
+const EXAMPLE = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    width: auto;
+    height: 50px;
+    color: #4E5A65;
+    font-size: 13px;
+`;
+const CIRCLE = styled.div`
+    height: 26px;
+    width: 26px;
+    border-radius: 12px;
+    background-color: ${props => props.color};
+`;
+const FORM = styled.form`
+    display: flex;
+    flex-direction: column;
+    font-size: 18px;
+    height: auto;
+    width: 100%;
+    margin: 0 24px 0 24px;
+`;
+const INPUTFORM = styled.input`
+    height: 50px;
+    width: 100%;
+    border: 1px solid #D4D4D4;
+    border-radius: 3px;
+    margin: 3px 0 10px 0;
+    ::placeholder{
+        color: #AFAFAF;
+        font-size: 18px;
+        font-style: italic;
+    }
+`;
+const BUTTONFORM = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 42px;
+    width: 225px;
+    border: none;
+    border-radius: 3px;
+    background-color: #E8833A;
+    margin: 60px auto 0 auto;
+    color: #FFFFFF;
+    font-size: 18px;
 `;
